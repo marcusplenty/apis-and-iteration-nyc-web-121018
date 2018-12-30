ba11af816c2473eca2ad5c0db6fda780ec3f8506
Marcus Plenty <marcusplenty@gmail.com>
2018-12-30 04:47:22 +0000

    Automatically backed up by Learn

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -18,7 +18,7 @@ def get_character_movies_from_api(character_name)
       end 
     end 
   end
-  final list = film_apis.map do {|url|RestClient.get(url)}
+  final list = film_apis.map {|url|RestClient.get(url)}
     
   # iterate over the response hash to find the collection of `films` for the given
   #   `character`
